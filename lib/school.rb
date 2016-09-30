68cbcc4f68d909f3cecd3b9e0854fcc935a929d4
Amanda Steckler <amandajsteckler@gmail.com>
2016-09-30 17:00:00 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.map do |grade, student_array|
+      roster[grade] = student_array.sort
+    end
+    roster
+  end
+end
